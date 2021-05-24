b8849b2fa588811b4c2a63a0f777bd0d94892ebe
James Rosas <jamesadrian777@gmail.com>
2021-05-24 11:13:49 -0500

    agregue la pagina de informacion de checkout con el formulario de datos para el usuario, y tenemos la lista del pedido

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import '../styles/componentes/Information.css'
+import initialState from '../initialState'
 
 
 function Information() {
   return (
-  <h1>soy la Information</h1>
+    <div className="information-container">
+      <h1>Tu Informacion</h1>
+      <div className="information-boxes">
+        <div className="form-box">
+          <h2>Informacion de contacto</h2>
+          <form>
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="email" placeholder="Correo Electronico" name="name" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Region" name="region" />
+            <input type="text" placeholder="codigo postal" name="postal" />
+            <input type="text" placeholder="Telefono" name="number" />            
+          </form>
+          <div className="buttons-container">
+            <button>Regresar</button>
+            <button>Pagar</button>
+          </div>
+        </div>
+        <div className="pedido-box">
+          <h2>Pedido</h2>
+          <div className="pedido-table">
+              <img width={80} src={initialState.products[0].image}/>
+              <p>$ 10</p>
+          </div>
+        </div>
+      </div>      
+    </div>
   )
 }
 
